c1294db64d3f049d147b254bc18573851e61cff2
Rajyaguru Nayan <nayanrajyaguru59@gamil.com>
2025-08-06 18:17:55 +0530

    some changes

--- backend/app.js
+++ backend/app.js
@@ -23,9 +23,5 @@ app.get("/", (req, res) => {
 
 app.use("/signup", require("./src/routes/user/signup.js"));
 app.use("/login", require("./src/routes/user/login.js"));
-app.use("/quiz", require("./src/routes/quiz/quiz.js"));
-app.use("/form", require("./src/routes/summrieze/form.js"));
-app.use("/profile", require("./src/routes/profile/profile.js"));
-app.use("/chat", require("./src/routes/chat/chat.js"));
 
 module.exports = app;
